9c8e5c83830cc76e37df4d8fe77ad6fbffe92750
Bipin Gurung <bipin67678grg@gmail.com>
2023-09-06 15:31:52 +0545

    htmlandcss folder structure

--- basicJs/task9/task9.js
+++
@@ -1,6 +0,0 @@
-const chechStringUpperCaseFun = (args) =>{
-    return args === args.toUpperCase()
-}
-
-console.log(chechStringUpperCaseFun('HELLO WORLD'));
-console.log(chechStringUpperCaseFun('bipin gurung'));
