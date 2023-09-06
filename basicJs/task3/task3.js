0990f5f108efe7814b9cd475c61e4f723413a7e6
Bipin Gurung <bipin67678grg@gmail.com>
2023-09-06 16:33:15 +0545

    basic js folder structure

--- basicJs/task3/task3.js
+++
@@ -1,9 +0,0 @@
-const newString = (str) => {
-  let result = "";
-  const arrayToString = str.split(" ");
-  for (let i = 0; i < arrayToString.length; i++) {
-    result += arrayToString[i][0];
-  }
-  return result
-};
-console.log(newString("Bipin Sulav Swekreet Biraj"));
