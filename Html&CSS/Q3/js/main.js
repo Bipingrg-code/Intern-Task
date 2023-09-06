13fed452df549fd89f5b839429f9c151bb3c603e
Bipin Gurung <bipin67678grg@gmail.com>
2023-09-06 14:59:00 +0545

    fixed issue of q3 and q5

--- Html&CSS/Q3/js/main.js
+++ Html&CSS/Q3/js/main.js
@@ -1,7 +1,8 @@
 //grab the value
 const nextBtn = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
-const formStep = document.querySelectorAll(".form-step");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
 
 let formStemCount = 0;
 
@@ -10,12 +11,30 @@ nextBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     formStemCount++;
     updateFormSteps();
+    upDateProgressBar();
   });
 });
+
+//next step data update
 function updateFormSteps() {
-  formStep.forEach((formstep) => {
-    formStep.classList.contains(".active") &&
-      formStep.classList.remove(".active");
+  formSteps.forEach((formstep) => {
+    formstep.classList.contains("active") &&
+      formstep.classList.remove("active");
   });
-  formStep[formStemCount].classList.add("active");
+  formSteps[formStemCount].classList.add("active");
+}
+
+//next step progress-steps update
+function upDateProgressBar() {
+  progressSteps.forEach((stage, id) => {
+    if (id < formStemCount + 1) {
+      stage.classList.add("active");
+    } else {
+      stage.classList.remove("active");
+    }
+  });
+  //progress-active
+  const progressActive = document.querySelectorAll(".progress-step.active");
+  progress.style.width =
+    ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
